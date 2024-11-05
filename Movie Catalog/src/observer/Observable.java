e83a48ca1bbef4706106847a33e09f61d8bae502
yErkaa <161349620+yErkaa@users.noreply.github.com>
2024-11-06 01:11:44 +0500

    Delete Movie Catalog directory

--- Movie Catalog/src/observer/Observable.java
+++
@@ -1,7 +0,0 @@
-package observer;
-
-public interface Observable {
-    void addObserver(Observer observer);
-    void removeObserver(Observer observer);
-    void notifyObservers(String message);
-}
--- Movie Catalog/src/utils/MovieFactory.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-import model.Movie;
-
-public class MovieFactory {
-    public static Movie createMovie(String title, int year, String genre, double rating) {
-        return new Movie(title, year, genre, rating);
-    }
-}
